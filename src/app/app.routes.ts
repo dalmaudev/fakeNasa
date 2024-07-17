fae821722361394e90812847cc0e39b6ac1417a8
Jordi Dalmau <jdalmaudev@gmail.com>
2024-07-17 17:30:12 +0200

    Routes config and redirects

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'register',
+    title: 'Register Page',
+    component: FormComponent,
+  },
+  {
+    path: 'login',
+    title: 'Login Page',
+    component: LoginComponent,
+  },
+  { path: '', redirectTo: '/register', pathMatch: 'full' },
+];
